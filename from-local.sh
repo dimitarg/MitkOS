85e6560c3c7eb7e6d7e1ef3dca7697b0ce81a31a
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2022-12-03 21:28:12 +0200

    More diagnostic output

--- from-local.sh
+++ from-local.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 echo "Copying nixos system configuration from local host to git repository, under ./hosts/$(hostname)"
-cp -a /etc/nixos/. "hosts/$(hostname)/"
+cp -a --verbose /etc/nixos/. "hosts/$(hostname)/"
 echo "Copying home manager configuration from local host to git repository"
-cp -a ~/.config/nixpkgs/. home-manager/
+cp -a --verbose ~/.config/nixpkgs/. home-manager/
 
 git status
--- to-local.sh
+++ to-local.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 echo "Copying nixos system configuration from ./hosts/$(hostname) to local host (this needs sudo)"
-sudo cp -a "hosts/$(hostname)/." /etc/nixos/
+sudo cp -a --verbose "hosts/$(hostname)/." /etc/nixos/
 echo "Copying home manager configuration from git repository to local host"
-cp -a home-manager/. ~/.config/nixpkgs/ 
+cp -a --verbose home-manager/. ~/.config/nixpkg
+
+echo "Dry build of system (nixos-rebuild) configuration:"
+nixos-rebuild dry-build
+
+echo ""
+echo "Dry build of the home manager configuration:"
+echo "Sorry, I don't know how to do that yet."
