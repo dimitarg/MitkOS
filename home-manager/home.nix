8faa907a86a452c126110fa2f39770cb8eeac049
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2022-12-04 14:01:29 +0200

    Fix wrong path in copy script; support zsh in nix-shell vanilla

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -78,12 +78,30 @@ in {
     };
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "aws" ];
+      plugins = [ 
+        "git"
+        "aws"
+      ];
       theme = "robbyrussell";
     };
     historySubstringSearch = {
       enable = true;
     };
+
+    enableCompletion = true;
+    
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
   };
 
   programs.direnv = {
--- to-local.sh
+++ to-local.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 echo "Copying nixos system configuration from ./hosts/$(hostname) to local host (this needs sudo)"
 sudo cp -a --verbose "hosts/$(hostname)/." /etc/nixos/
 echo "Copying home manager configuration from git repository to local host"
-cp -a --verbose home-manager/. ~/.config/nixpkg
+cp -a --verbose home-manager/. ~/.config/nixpkgs
 
 echo "Dry build of system (nixos-rebuild) configuration:"
 nixos-rebuild dry-build
