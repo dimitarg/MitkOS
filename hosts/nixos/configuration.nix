77f556b6251c9d5362347781999b463677269bbd
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2023-10-30 15:21:43 +0200

    steam

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -206,6 +206,12 @@
     pinentryFlavor = "gnome3";
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
