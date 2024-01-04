dc5ed82c570866f1f17572df9b8bcb950d5da59a
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2024-01-05 00:03:17 +0200

    cli-compatibility
    Keep updating "channels" so that non-flake CLIs are happy.

--- update.sh
+++ update.sh
@@ -3,3 +3,9 @@
 set -euo pipefail
 
 nix flake update
+
+# this does not affect the system derivation / build
+# nix channels are only used in non-flakified CLIs such as nix-shell , etc
+# this update is only done in order to keep flake.lock and channels somewhat consistent, until we figure out a better solution
+nix-channel --update
+sudo nix-channel --update
