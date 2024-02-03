0fe2dc07ff5b430a6731553913416b7b919899bd
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2024-02-03 17:30:56 +0200

    Experimental tlp config

--- system-common/modules/laptop.nix
+++ system-common/modules/laptop.nix
@@ -2,7 +2,13 @@
 
 {
   services.power-profiles-daemon.enable = false; # not recommended to use power-profiles-daemon together with tlp
-  services.tlp.enable = true; # todo configure
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     powertop # just the package / cli for monitoring purposes, not the service / auto-tuning functionality, as that conflicts with tlp
