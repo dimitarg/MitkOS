d53517135b1cfe5b6db46e7b3d08db407f111c7c
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2024-03-25 13:18:31 +0200

    updates

--- system-common/sys.nix
+++ system-common/sys.nix
@@ -187,7 +187,7 @@
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    pinentryFlavor = "gnome3";
+    pinentryPackage = pkgs.pinentry-gnome3;
   };
 
   # List services that you want to enable:
