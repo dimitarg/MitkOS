8e5adc5fefe4ea1d91adf0131a0aaf01ed9ab25f
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2024-04-30 11:21:14 +0300

    updates

--- system-common/sys.nix
+++ system-common/sys.nix
@@ -105,7 +105,7 @@
 
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
   
   
   # enable docker. This will auto-start dockerd
