eee961a7ff3fca5461259158b4ccbaddfcfdea27
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2025-04-19 15:33:36 +0300

    Enable nixos-hardware alder lake settings

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -5,7 +5,14 @@
     ./hardware-configuration.nix
     ./from-install.nix
     ../../system-common/modules/intel-laptop.nix
-    inputs.nixos-hardware.nixosModules.common-gpu-intel
+    
+    # inputs.nixos-hardware.nixosModules.common-gpu-intel
+
+    # this profile is imported as path and not a flake output, since common profiles are getting deprecated
+    # , because they're considered for moving in nixpkgs directly
+    # this profile also enables the intel alder lake GPU profile
+    # notably it switches the driver to `intel-media-driver`
+    "${inputs.nixos-hardware}/common/cpu/intel/alder-lake" 
   ];
 
 }
