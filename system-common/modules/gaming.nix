237392b57ba003d16ee5a33974a285acd7806457
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2025-05-20 10:16:44 +0300

    Remove unused steam config

--- system-common/modules/gaming.nix
+++ system-common/modules/gaming.nix
@@ -4,8 +4,8 @@
   config = lib.mkIf osConfig.gaming.enable {
     programs.steam = {
       enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
   };
 }
