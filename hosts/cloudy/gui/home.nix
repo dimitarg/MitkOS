784c712a2d7ef78560af33d3eecaa5b123914af3
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2025-06-11 18:04:00 +0300

    Really really don't suspend cloudy

--- hosts/cloudy/gui/home.nix
+++ hosts/cloudy/gui/home.nix
@@ -12,7 +12,14 @@
     "org.gnome.settings-daemon.plugins.power" = {
       "sleep-inactive-battery-type" = "nothing";
       "sleep-inactive-ac-type" = "nothing";
+      "idle-dim" = false;
+      "sleep-inactive-battery-timeout" = 0;
+      "sleep-inactive-ac-timeout" = 0;
     };
+    "org.gnome.desktop.session" = {
+      "idle-delay" = 0;
+    };
+
   };
 
 }
